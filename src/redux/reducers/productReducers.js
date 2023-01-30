cce60c2401e7fc65f1460bb0c9f144e6e216df06
Md Sholayman <mdsholayman998@gmail.com>
2023-01-31 00:38:14 +0600

    implementing redux

---
+++ src/redux/reducers/productReducers.js
@@ -0,0 +1,9 @@
+
+const initialState = {
+    products: [],
+  };
+const productReducers = (state = initialState, action) =>{
+
+}
+
+export default productReducers
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import productReducers from "./reducers/productReducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+
+const store = createStore(productReducers , composeWithDevTools() )
+
+export default store
